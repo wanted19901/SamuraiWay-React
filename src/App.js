4d90d7133fa866faefb7876bf806d69620ba69b1
wanted19901 <wanted19901@gmail.com>
2020-03-23 18:39:14 +0300

    1st step on project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import React from 'react';
 import './App.css';
 
+
 const App = () => {
     return (
-        <div>
-             <Header />
-             <Tehnologies />
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRey9X_lVOBr1fSb_uayt-imi2S2fpC2qQm3YpnKMAZshH33OPM'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Masage
+                </div>
+                <div>
+                    News
+                </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Setting
+                </div>
+            </nav>
+            <div className='content'>
+                Main content
+                <img src='https://photar.ru/wp-content/uploads/2019/08/0-4.jpg'/>
+                <div className='Profile'>
+                    <div className='profilePhoto'><img src='https://bipbap.ru/wp-content/uploads/2017/10/3-6.png'/>
+                    </div>
+                    <div className='ProfileInfo'>
+                        <div className='Name'> Aleksey G.</div>
+                        <div className='AboutMe'>
+                            <ul>
+                                <li>Date of birthday:</li>
+                                <li> City:</li>
+                                <li> Aducation:</li>
+                                <li> Web-site:</li>
+                            </ul>
+
+                        </div>
+
+
+                    </div>
+                </div>
+                <div className='MyPost'> My post
+                    <div className='NewPost'> New post</div>
+                </div>
+            </div>
         </div>
     );
 }
 
-const Tehnologies = () => {
-  return(
-      <div >
-        <ul>
-          <li>HTML</li>
-          <li>react</li>
-          <li>css</li>
-        </ul>
-      </div>
-  )
-}
-const Header = () => {
-    return (
-        <div>
-            <ul>
-                <a href={1}>Menu</a>
-                <a href={2}>Contact</a>
-                <a href={3}>Order</a>
-            </ul>
-        </div>
-    );
-}
 
 export default App;
