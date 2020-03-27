f0ec076edeae53dc4d34857bf6a9818b51638c76
wanted19901 <wanted19901@gmail.com>
2020-03-27 12:56:52 +0300

    Refacroring page Profile

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import p from './ProfileInfo.module.css'
+
+
+const profileInfo = (props) => {
+    return (
+    <div className={p.Profile}>
+        <img className={p.img} src='https://photar.ru/wp-content/uploads/2019/08/0-4.jpg'/>
+        <div className={p.ProfileInfo}>
+            <img className={p.profilePhoto} src='https://bipbap.ru/wp-content/uploads/2017/10/3-6.png'/> 
+            <div className={p.Notification}>
+                <div className={p.Name}> Aleksey G.</div>
+                <div className={p.AboutMe}>
+                    <ul>
+                        <li>Date of birthday:</li>
+                        <li> City:</li>
+                        <li> Aducation:</li>
+                        <li> Web-site:</li>
+                    </ul>
+                </div>
+            </div>
+       
+        </div>  
+    
+
+
+    </div>
+    )
+}
+export default profileInfo;
--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,28 +1,15 @@
 import React from "react";
 import p from './profile.module.css'
 import MyPosts from '../Profile/MyPosts/MyPosts'
+import ProfileInfo from './ProfileInfo/ProfileInfo'
 
 const Profile = () => {
     return <div >
-        <img className={p.img} src='https://photar.ru/wp-content/uploads/2019/08/0-4.jpg'/>
-        <div className={p.Profile}>
-            <img className={p.profilePhoto} src='https://bipbap.ru/wp-content/uploads/2017/10/3-6.png'/>
+        
+        
            
-            <div className={p.ProfileInfo}>
-                <div className={p.Name}> Aleksey G.</div>
-                <div className={p.AboutMe}>
-                    <ul>
-                        <li>Date of birthday:</li>
-                        <li> City:</li>
-                        <li> Aducation:</li>
-                        <li> Web-site:</li>
-                    </ul>
-
-                </div>
-
-
-            </div>
-        </div>
+       <ProfileInfo />
+        
        <MyPosts/>
 
     </div>
